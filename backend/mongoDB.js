33444fc25604b14a460b30bf0c7bbd57f7b231b9
Jason Kwan <j-kwan@outlook.com>
2023-04-16 15:59:17 -0400

    Added DB helper functions, Query, DocumentExists, DB insertion

--- backend/mongoDB.js
+++ backend/mongoDB.js
@@ -7,6 +7,11 @@ async function main() {
     try {
       await client.connect();
       await listDatabases(client);
+      console.log(await UserExists(client, 01));
+      console.log("Passed");
+      await databaseInsertion(client, "middleName", "Arek", 01);
+      console.log("Second Pass");
+      console.log(await databaseQuery(client, "middleName", 1));
     } catch (e) {
         console.error(e);
     } finally {
@@ -30,3 +35,37 @@ async function listDatabases(client){
   };
 
 
+  async function databaseInsertion(client, field, value, userID) {
+    console.log("Insertion of a field and value into a Database")
+    field = field;
+    value = value;
+    userID = userID;
+    await client.db("Users").collection("Users").updateOne( {_id: userID},
+      {
+        $set: {[field]: value}
+      });
+  }
+
+  async function databaseQuery(client, field, userID) {
+    console.log("Query for data")
+    field = field;
+    userID = userID;
+
+    // val = await client.db("Users").collection("Users").find({_id: userID}, {[field] : 1})
+    try {
+      const result = await client.db("Users").collection("Users").findOne({_id: userID});
+      return result.field;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async function UserExists(client, userID) {
+    console.log('Checking if this User Exists')
+    count =  await client.db("Users").collection("Users").find( {_id: userID} ).count()
+    return count
+  }
+
+
+// state, access token, refresh token, userID, likedArtists
+// String, String, String, String, String[]
