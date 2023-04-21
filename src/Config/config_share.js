586ee253d5c2f28a0ea60d81240ad175f93f4c09
Jason Kwan <j-kwan@outlook.com>
2023-04-21 13:32:21 -0400

    Added mongoDB_APIKey

--- src/Config/config_share.js
+++ src/Config/config_share.js
@@ -3,7 +3,7 @@
 //make sure gitignore is ignoring this file
 //add your keys and secrets to this file
 //create a variable mongoDB
-const mongoDB_API = 'null'
+const mongoDB_API = "mongodb+srv://Jkwan:21323002448232@cluster0.dqni373.mongodb.net/test"
 //create a varuable spotify api
 const spotify_client = 'null'
 const spotify_secret = 'null'
