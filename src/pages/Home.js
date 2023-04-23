5e4ccf977b18c6124a688e334d720e7055975ea0
marzbana <marza@bu.edu>
2023-04-23 18:44:20 -0400

    added home buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,8 @@ function Home() {
   if(!checkCookie()) {
   return (
     <div>
-      <div style={{position: 'absolute', top: 0, right: 0}}>
-        <Link to="/login"><button>Login</button></Link>
-      </div>
       <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-        <Link to='/search?loggedIn=false'><button>Search</button></Link>
+        <Link to="/login/loggedIn=false"><button>Login</button></Link>
       </div>
     </div>
   );
@@ -19,10 +16,10 @@ else {
   return (
     <div>
       <div style={{position: 'absolute', top: 0, right: 0}}>
-        <Link to="/logout"><button>Log Out</button></Link>
+        <Link to="/logout/loggedIn=true"><button>Log Out</button></Link>
       </div>
       <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-        <Link to='/search?loggedIn=true'><button>Search</button></Link>
+        <Link to='/search/loggedIn=true'><button>Search</button></Link>
       </div>
     </div>
   );
